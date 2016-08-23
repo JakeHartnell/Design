61800a4c85457e2f10dc219007f06ed451e66665
Jake Hartnell <Jake.Hartnell@gmail.com>
2016-08-22 21:09:34 -0700

    Lot's of deleting.

--- HTML/One-Pages/Charity/assets/js/plugins/success-stories.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.success-stories').slick({
-    infinite: true,
-    slidesToShow: 1,
-    arrows: true,
-    prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left"></i></div>',
-    nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right"></i></div>',
-  });
-});
